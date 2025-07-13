60a5c872f3529733ca4ed4b7c70c202f0271eceb
Happy <muvunyiedwin988@gmail.com>
2025-07-13 16:55:50 +0300

    Add files via upload

---
+++ details.php
@@ -0,0 +1,115 @@
+<html lang="en">
+
+<head>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dynasoft Software</title>
+</head>
+
+<body class="border border-gray-500 bg-white-300 rounded-[2%]  w-[520px] h-[580px] mt-10 ml-20 shadow">
+    <div>
+        <div class="text-3xl border-b px-5  ">
+            <h1>Task Details</h1>
+        </div>
+    </div>
+
+    <div class="text-xl ml-5 py-6 font-bold ">
+        <p>Data Design Exercise</p>
+    </div>
+    <div class="flex justify-center">
+        <div class="bg-blue-200 text-blue-800 rounded-[10%] h-[4vh]  w-[110px] mr-8 px-3 py-0 ml-5 mb-5">
+            in progress
+        </div>
+        <div class="mr-20">
+            .Assigned on 6/20/2023
+        </div>
+    </div>
+
+    <div class="text-s ml-6  mb-5 font-bold">
+        <h2>Description</h2>
+    </div>
+
+    <div class="ml-6">
+        <p>Design and normalize a database for an e-commerce system.<br>
+            Include Entity Relation Diagrams
+        </p>
+    </div>
+
+    <div class="grid grid-cols-3">
+
+        <div class="ml-6 mt-6">
+            <div class="font-bold">
+                Due Date
+
+            </div>
+
+            <div>
+                7/10/2023
+            </div>
+        </div>
+
+        <div class="ml-6 mt-6">
+            <div class="font-bold">
+                Priolity
+
+            </div>
+
+            <div>
+                medium
+            </div>
+        </div>
+
+
+        <div class="ml-6 mt-6">
+            <div class="font-bold">
+                Trainer
+
+            </div>
+
+            <div>
+                Jane doe
+            </div>
+        </div>
+
+    </div>
+
+    <h3 class="ml-6 mt-4 mb-4">
+        <p class="font-bold ">Your Subscription</p>
+    </h3>
+
+    <div class="bg-gray-200 rounded-[%] ml-4 mr-4">
+
+        <div class="ml-3">
+            06/21/2023,2:00:00AM
+        </div>
+
+        <div class="ml-3">
+            <p>Initial draftof schema design</p>
+        </div>
+
+        <div class="ml-3"><svg class="inline-block w-[20px] mb-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+            </svg>
+
+            schema_drive.pdf
+        </div>
+    </div>
+
+    <div class="flex justify-end">
+        <div class="bg-white-200 mr-6 mt-7 text-xl ">
+            <button>Close</button>
+        </div>
+
+        <div class=" py-2 px-2 text-xl text-white-300 border border-blue-600 w-[200px] h-[50px] bg-blue-600 rounded mt-5 mr-6">
+            <button>
+                Update Submission
+            </button>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
